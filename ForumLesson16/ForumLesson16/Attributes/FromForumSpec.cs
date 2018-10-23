396bf33091b4d9584f20d185fd1506bc2697a6e2
zadykian <zadykian@outlook.com>
2018-10-23 22:03:53 +0300

    Added classes

---
+++ ForumLesson16/ForumLesson16/Attributes/FromForumSpec.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumLesson16.Attributes
+{
+    public class FromForumSpec : Attribute
+    {
+    }
+}
