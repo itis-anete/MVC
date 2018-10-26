08f0240007f168fb6f1bd854078afe419f26f4aa
d4n0n_myself <danon.sibaev@gmail.com>
2018-10-27 00:10:41 +0300

    Not using this

--- Route/ControllerSupport/ActionDescriptionProvider.cs
+++ Route/ControllerSupport/ActionDescriptionProvider.cs
@@ -1,38 +1,38 @@
-using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc.Abstractions;
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
-using Microsoft.AspNetCore.Mvc.Internal;
-
-namespace Route
-{
-    public class ActionDescriptorProvider : IActionDescriptorProvider
-    {
-        public int Order { get; }
-        private readonly IEnumerable<IApplicationModelConvention> _conventions;
-
-        public void OnProvidersExecuting(ActionDescriptorProviderContext context)
-        {
-            var model = new ApplicationModelProviderContext(null).Result;
-            ApplicationModelConventions.ApplyConventions(model, _conventions);
-            foreach (var e in ControllerActionDescriptorBuilder.Build(model))
-            {
-                context.Results.Add(e);
-            }
-        }
-
-        public void OnProvidersExecuted(ActionDescriptorProviderContext context)
-        {
-            var keys = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
-
-            foreach (var action in context.Results)
-            foreach (var key in action.RouteValues.Keys)
-                keys.Add(key);
-
-            foreach (var action in context.Results)
-            foreach (var key in keys)
-                if (!action.RouteValues.ContainsKey(key))
-                    action.RouteValues.Add(key, null);
-        }
-    }
-}
+//using System;
+//using System.Collections.Generic;
+//using Microsoft.AspNetCore.Mvc.Abstractions;
+//using Microsoft.AspNetCore.Mvc.ApplicationModels;
+//using Microsoft.AspNetCore.Mvc.Internal;
+//
+//namespace Route
+//{
+//    public class ActionDescriptorProvider : IActionDescriptorProvider
+//    {
+//        public int Order { get; }
+//        private readonly IEnumerable<IApplicationModelConvention> _conventions;
+//
+//        public void OnProvidersExecuting(ActionDescriptorProviderContext context)
+//        {
+//            var model = new ApplicationModelProviderContext(null).Result;
+//            ApplicationModelConventions.ApplyConventions(model, _conventions);
+//            foreach (var e in ControllerActionDescriptorBuilder.Build(model))
+//            {
+//                context.Results.Add(e);
+//            }
+//        }
+//
+//        public void OnProvidersExecuted(ActionDescriptorProviderContext context)
+//        {
+//            var keys = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+//
+//            foreach (var action in context.Results)
+//            foreach (var key in action.RouteValues.Keys)
+//                keys.Add(key);
+//
+//            foreach (var action in context.Results)
+//            foreach (var key in keys)
+//                if (!action.RouteValues.ContainsKey(key))
+//                    action.RouteValues.Add(key, null);
+//        }
+//    }
+//}
--- Route/ControllerSupport/InnerCache.cs
+++ Route/ControllerSupport/InnerCache.cs
@@ -1,19 +1,19 @@
-using System.Collections.Concurrent;
-using Microsoft.AspNetCore.Mvc.Abstractions;
-using Microsoft.AspNetCore.Mvc.RazorPages.Internal;
-
-namespace Route
-{
-    internal class InnerCache
-    {
-        public int Version { get; }
-        
-        public InnerCache(int version)
-        {
-            Version = version;
-        }
-
-        public ConcurrentDictionary<ActionDescriptor, PageActionInvokerCacheEntry> Entries { get; } =
-            new ConcurrentDictionary<ActionDescriptor, PageActionInvokerCacheEntry>();
-    }
-}
+//using System.Collections.Concurrent;
+//using Microsoft.AspNetCore.Mvc.Abstractions;
+//using Microsoft.AspNetCore.Mvc.RazorPages.Internal;
+//
+//namespace Route
+//{
+//    internal class InnerCache
+//    {
+//        public int Version { get; }
+//        
+//        public InnerCache(int version)
+//        {
+//            Version = version;
+//        }
+//
+//        public ConcurrentDictionary<ActionDescriptor, PageActionInvokerCacheEntry> Entries { get; } =
+//            new ConcurrentDictionary<ActionDescriptor, PageActionInvokerCacheEntry>();
+//    }
+//}
