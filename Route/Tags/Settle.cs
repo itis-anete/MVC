70caae2f6fb8501647708aef4b96a2d77c3b5a00
Rishat Latfullin <Marearcat@outlook.com>
2018-10-26 15:56:35 +0300

    +Settle tag, TypeButton HtmlHelper

---
+++ Route/Tags/Settle.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Route.Tags
+{
+    public class Settle : TagHelper
+    {
+        public string Address { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", Address);
+            output.Content.SetContent("Следи за нами в github");
+        }
+    }
+}
