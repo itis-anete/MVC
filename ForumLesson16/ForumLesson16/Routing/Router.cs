ffa90bd0ffc11979e856cc57caad6af89cdfc393
zadykian <zadykian@outlook.com>
2018-10-27 12:27:25 +0300

    useless

--- ForumLesson16/ForumLesson16/Routing/Router.cs
+++ ForumLesson16/ForumLesson16/Routing/Router.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Routing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ForumLesson16.Routing
+namespace ForumLesson16
 {
-    public class Router
+    public class HtmlMethodRouter : IRouter
     {
+        private readonly IRouter defaultRouter;
+
+        public HtmlMethodRouter(IRouter defaultRouter)
+        {
+            this.defaultRouter = defaultRouter;
+        }
+
+        public VirtualPathData GetVirtualPath(VirtualPathContext context) =>
+            defaultRouter.GetVirtualPath(context);
+
+        public async Task RouteAsync(RouteContext context)
+        {
+            
+            var urlTokens = context.HttpContext.Request.Path.Value
+                .TrimStart('/')
+                .TrimEnd('/')
+                .Split('/');
+
+            if (urlTokens.Length == 2)
+            {
+                context.RouteData.Values["controller"] = urlTokens[0].Substring(6, urlTokens[0].Length - 16);
+                context.RouteData.Values["action"] = "";
+                await defaultRouter.RouteAsync(context);
+            }
+        }
     }
 }
